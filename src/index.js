826d44dec76854d7f7ae709cc247b4bef47d8228
shusillimbu <shusillimbupangdhak@gmail.com>
2023-02-19 09:09:57 +0545

    third commit

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
+    <BrowserRouter basename='/Shusil_Pangdhak.github.io'>
+    <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
